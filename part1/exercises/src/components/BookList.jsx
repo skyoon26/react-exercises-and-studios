eae9d15d2d0db7edc3c1ad6715fc818eac70a817
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-03-11 13:53:16 -0500

    Modify CSS for movie and chores lists and complete book list component with working images

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "My Books";
+   let book1 = "https://m.media-amazon.com/images/I/61GldX4MpUL._SL1500_.jpg";
+   let book2 = "https://s26162.pcdn.co/wp-content/uploads/2023/01/9780374606244-668x1024.jpg";
+   let book3 = "https://s26162.pcdn.co/wp-content/uploads/2023/12/9781784878771-630x1024.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Happy by Celina Baljeet Basra" />
+         <img src={book2} alt="The Sun Walks Down by Fiona McFarlane" />
+         <img src={book3} alt="A Wild Sheep Chase by Haruki Murakami" />
       </div>      
    );
 }
